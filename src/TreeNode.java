de2a33ea7b72bc5465505932e50055b04a7e8c5a
Fardinolif <71824238+Fardinolif@users.noreply.github.com>
2023-03-05 11:05:36 +0100

    Initial commit

---
+++ src/TreeNode.java
@@ -0,0 +1,12 @@
+public class TreeNode {
+
+	public Integer value;
+	public TreeNode left;
+	public TreeNode right;
+
+	public TreeNode(Integer value){
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
